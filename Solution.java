04e72c3b0ca11e8a843687ec1d23ac29d5803748
LokithR <123667526+LokithR@users.noreply.github.com>
2023-01-27 21:50:09 +0530

    initial commit

---
+++ Solution.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+	public static void main(String[] args) {
+
+		// write a java program to implement post increment operator and pre incremenet
+		// operator
+
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		// int y=sc.nextInt();
+		float z = sc.nextFloat();
+
+		/*
+		 * int z=sc.nextInt();
+		 * 
+		 * int a=6,b=7;
+		 * System.out.println(++a);
+		 * System.out.println(--b);
+		 * 
+		 * 
+		 * // java program to find the max between 3 numbers;
+		 * 
+		 * 
+		 * int max =0;
+		 * max = x>y?((x>z)?x:z):(y>z)?y:z;
+		 * System.out.println(max);
+		 * 
+		 * System.out.println(~x);
+		 * System.out.println(x&y);
+		 * System.out.println(x^y);
+		 * System.out.println(x>>2);
+		 * System.out.println(x<<2);
+		 * 
+		 * // java program to check if a num is even odd
+		 * 
+		 * if(x%2==0){
+		 * System.out.println("even");
+		 * }
+		 * else System.out.println("odd");
+		 * 
+		 * if(x<y){
+		 * System.out.println(y);
+		 * }
+		 * else System.out.println(x);
+		 */
+
+	}
+}
